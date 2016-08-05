544a5842eb5c9c69ecbcaf9927bf082cfc812f82
shibdib <supernfadmin@gmail.com>
2016-08-04 18:21:50 -0600

    report eve name when roles removed

--- src/plugins/onTick/authCheck.php
+++ src/plugins/onTick/authCheck.php
@@ -135,11 +135,11 @@ class authCheck
                                 }
 
                                 // Send the info to the channel
-                                $msg = $eveName . " roles have been removed via the auth.";
+                                $msg = "{$eveName} roles have been removed via the auth.";
                                 $channelID = $toDiscordChannel;
                                 $channel = Channel::find($channelID);
                                 $channel->sendMessage($msg, false);
-                                $this->logger->addInfo($eveName . " roles ({$role}) have been removed via the auth.");
+                                $this->logger->addInfo("{$eveName} roles ({$role}) have been removed via the auth.");
 
                                 $sql = "UPDATE authUsers SET active='no' WHERE discordID='$discordID'";
                                 $conn->query($sql);
