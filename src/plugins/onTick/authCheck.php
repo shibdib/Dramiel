f5567d7f3561f414becc44650d4865fde6a00b03
shibdib <supernfadmin@gmail.com>
2016-09-11 19:47:59 -0400

    check added for guildID issues to fix #33

--- src/plugins/onTick/authCheck.php
+++ src/plugins/onTick/authCheck.php
@@ -23,7 +23,6 @@
  * SOFTWARE.
  */
 
-use discord\discord;
 
 /**
  * Class fileAuthCheck
@@ -120,8 +119,18 @@ class authCheck
         //get bot ID so we don't remove out own roles
         $botID = $this->discord->id;
 
-        //Remove members who have roles but never authed
+        //Get guild object
         $guild = $discord->guilds->get('id', $id);
+
+        //Check to make sure guildID is set correctly
+        if (is_null($guild)){
+            $this->logger->addError("Config Error: Ensure the guild entry in the config is the guildID (aka serverID) for the main server that the bot is in.");
+            $nextCheck = time() + 7200;
+            setPermCache("authLastChecked", $nextCheck);
+            return null;
+        }
+
+        //Remove members who have roles but never authed
         foreach($guild->members as $member) {
             $id = $member->id;
             $username = $member->username;
