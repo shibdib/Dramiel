a9bf534ecace1e03d5a34c7023c5c9fd63804894
shibdib <supernfadmin@gmail.com>
2016-09-11 16:44:39 -0400

    Remove auto update, tweak notifications

--- src/lib/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Absolute path 
-SCRIPT=$(readlink -f "$0")
-# Absolute path 
-SCRIPTPATH=$(dirname "$SCRIPT")
-cd $SCRIPTPATH
-git -C ../.. pull origin dev
-composer update -d ../..
-echo "1"
