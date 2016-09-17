576474c6a8d1df14a410ed5e4643c803167246a6
shibdib <supernfadmin@gmail.com>
2016-09-17 00:19:02 -0400

    user agent

--- src/lib/eveApi.php
+++ src/lib/eveApi.php
@@ -42,6 +42,7 @@ function makeApiRequest($url)
             CURLOPT_FOLLOWLOCATION => true, // Yes, we want to follow a redirect
             CURLOPT_RETURNTRANSFER => true, // Yes, we want that curl_exec returns the fetched data
             CURLOPT_SSL_VERIFYPEER => false, // Do not verify the SSL certificate
+            CURLOPT_USERAGENT => 'Dramiel Discord Bot - https://github.com/shibdib/Dramiel', // Useragent
             CURLOPT_TIMEOUT => 15,
         ));
         // Fetch the data from the URL
