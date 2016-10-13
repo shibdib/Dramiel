67f1c835c66cdbfc821cd85197b5c8775868ce71
hiveliberty <cyberlibertyx@gmail.com>
2016-10-14 00:41:45 +0400

    Checking more than one CorpID\AllianceID
    Some changes for checking more than one CorpID\AllianceID.
    Ability to turn on\off debug output to log.

--- src/plugins/onTick/evemails.php
+++ src/plugins/onTick/evemails.php
@@ -73,6 +73,11 @@ class evemails {
      * @var
      */
     var $keys;
+    /**
+     * @var
+     */
+    var $evemailsConf;
+
     public $keyID;
     public $vCode;
     public $guild;
@@ -88,20 +93,39 @@ class evemails {
         $this->config = $config;
         $this->discord = $discord;
         $this->logger = $logger;
-        $this->toIDs = $config["plugins"]["evemails"]["fromIDs"];
-        $this->toDiscordChannel = $config["plugins"]["evemails"]["channelID"];
-        $this->newestMailID = getPermCache("newestCorpMailID");
+		$this->evemailsConf = $config["plugins"]["evemails"];
         $this->maxID = 0;
-        $this->keyID = $config["eve"]["apiKeys"]["user1"]["keyID"];
-        $this->vCode = $config["eve"]["apiKeys"]["user1"]["vCode"];
         $this->guild = $config["bot"]["guild"];
-        $this->characterID = $config["eve"]["apiKeys"]["user1"]["characterID"];
         $this->nextCheck = 0;
-        $lastCheck = getPermCache("mailLastChecked{$this->keyID}");
-        if ($lastCheck == NULL) {
+		
+		foreach ($this->evemailsConf as $selected){
+			foreach ( $selected as $key => $value){
+				switch ($key) {
+					case "fromID":
+						break;
+					case "channelID":
+						break;
+					case "keyID":
+						$this->keyID = $value;
+						$lastCheck = getPermCache("mailLastChecked{$this->keyID}");
+						if ($lastCheck == NULL) {
+							// Schedule it for right now if first run
+							setPermCache("mailLastChecked{$this->keyID}", time() - 5);
+						}
+						break;
+					case "vCode":
+						break;
+					case "characterID":
+						break;
+				}
+			}
+		}
+		
+        //$lastCheck = getPermCache("mailLastChecked{$this->keyID}");
+        //if ($lastCheck == NULL) {
             // Schedule it for right now if first run
-            setPermCache("mailLastChecked{$this->keyID}", time() - 5);
-        }
+        //    setPermCache("mailLastChecked{$this->keyID}", time() - 5);
+        //}
     }
 
     /**
@@ -109,30 +133,71 @@ class evemails {
      */
     function tick()
     {
-        $lastChecked = getPermCache("mailLastChecked{$this->keyID}");
-        $keyID = $this->keyID;
-        $vCode = $this->vCode;
-        $characterID = $this->characterID;
-        $discord = $this->discord;
+        
+		foreach ($this->evemailsConf as $selected){
+			foreach ( $selected as $key => $value){
+				switch ($key) {
+					case "fromID":
+						$this->toIDs = $value;
+						break;
+					case "channelID":
+						$this->toDiscordChannel = $value;
+						break;
+					case "keyID":
+						$this->keyID = $value;
+						break;
+					case "vCode":
+						$this->vCode = $value;
+						break;
+					case "characterID":
+						$this->characterID = $value;
+						break;
+				}
+				
+			}
+			$lastChecked = getPermCache("mailLastChecked{$this->keyID}");
+			$this->newestMailID = getPermCache("newestCorpMailID{$this->keyID}");
+			$keyID = $this->keyID;
+			$vCode = $this->vCode;
+			$characterID = $this->characterID;
+			$discord = $this->discord;
+			if (DEBUG) {$temptoIDs = array(); $temptoIDs = $this->toIDs;}
+			if ($lastChecked <= time()) {
+				$this->logger->addInfo("Checking API Key {$keyID} for new mail..");
+				if (DEBUG) {$this->logger->addInfo("Debug MSG fTick-01-{$keyID}: {$temptoIDs[0]}, {$this->keyID}, {$this->vCode}, {$this->characterID}, {$this->newestMailID}");}
+				$this->checkMails($keyID, $vCode, $characterID, $discord);
+			}
+		}
+		
+		//$lastChecked = getPermCache("mailLastChecked{$this->keyID}");
+        //$keyID = $this->keyID;
+        //$vCode = $this->vCode;
+        //$characterID = $this->characterID;
+        //$discord = $this->discord;
 
-        if ($lastChecked <= time()) {
-            $this->logger->addInfo("Checking API Key {$keyID} for new mail..");
-            $this->checkMails($keyID, $vCode, $characterID, $discord);
-        }
+        //if ($lastChecked <= time()) {
+        //    $this->logger->addInfo("Checking API Key {$keyID} for new mail..");
+        //    $this->checkMails($keyID, $vCode, $characterID, $discord);
+        //}
 
     }
 
     function checkMails($keyID, $vCode, $characterID, $discord)
     {
-        $url = "https://api.eveonline.com/char/MailMessages.xml.aspx?keyID={$keyID}&vCode={$vCode}&characterID={$characterID}";
+		if (DEBUG) {$this->logger->addInfo("Debug MSG fCheckMails-01-{$keyID}: {$temptoIDs[0]}, {$keyID}, {$vCode}, {$characterID}, ");}
+		$url = "https://api.eveonline.com/char/MailMessages.xml.aspx?keyID={$keyID}&vCode={$vCode}&characterID={$characterID}";
+		if (DEBUG) {$this->logger->addInfo("Debug MSG fCheckMails-02-{$keyID}: {$url}");}
         $data = json_decode(json_encode(simplexml_load_string(downloadData($url), "SimpleXMLElement", LIBXML_NOCDATA)), true);
+		if (DEBUG) {$this->logger->addInfo("Debug MSG fCheckMails-03-{$keyID}: {$data}");}
         $data = $data["result"]["rowset"]["row"];
         $xml = makeApiRequest($url);
         $cached = $xml->cachedUntil[0];
         $baseUnix = strtotime($cached);
         $cacheClr = $baseUnix - 13500;
+		if (DEBUG) {$this->logger->addInfo("Debug MSG fCheckMails-04-{$keyID}: {$cached}, {$cacheClr}");}
         if ($cacheClr <= time()) {
-            $weirdTime = time() + 1830;
+            //$weirdTime = time() + 1830;
+			$weirdTime = time() + 130;
             $cacheTimer = gmdate("Y-m-d H:i:s", $weirdTime);
             setPermCache("mailLastChecked{$keyID}", $weirdTime);
         } else {
@@ -157,8 +222,10 @@ class evemails {
                 $title = $mail["title"];
                 $sentDate = $mail["sentDate"];
                 $url = "https://api.eveonline.com/char/MailBodies.xml.aspx?keyID={$keyID}&vCode={$vCode}&characterID={$characterID}&ids=" . $mail["messageID"];
-                $content = strip_tags(str_replace("<br>", "\n", json_decode(json_encode(simplexml_load_string(downloadData($url), "SimpleXMLElement", LIBXML_NOCDATA)))->result->rowset->row));
-
+				$content = strip_tags(str_replace("<br>", "\n", json_decode(json_encode(simplexml_load_string(downloadData($url), "SimpleXMLElement", LIBXML_NOCDATA)))->result->rowset->row));
+				if (DEBUG) {$this->logger->addInfo("Debug MSG fCheckMails-05-{$keyID}: {$url}");}
+				if (DEBUG) {$this->logger->addInfo("Debug MSG fCheckMails-06-{$keyID}: {$content}");}
+				
                 // Blank Content Check
                 if ($content == "") {
                     return null;
@@ -179,6 +246,7 @@ class evemails {
                 $guild = $discord->guilds->get('id', $this->guild);
                 $channel = $guild->channels->get('id', $channelID);
                 $channel->sendMessage($msg, false);
+				if (DEBUG) {$this->logger->addInfo("Debug MSG fCheckMails-07-{$keyID}: {$channelID}, {$guild}, {$channel}");}
                 sleep(1); // Lets sleep for a second, so we don't rage spam
                 if (strlen($content) > 1850) {
                     $channel->sendMessage($msgLong, false);
@@ -187,15 +255,17 @@ class evemails {
                 // Find the maxID so we don't spit this message out ever again
                 $this->maxID = max($mail["messageID"], $this->maxID);
                 $this->newestMailID = $this->maxID; //$mail["messageID"];
+				if (DEBUG) {$this->logger->addInfo("Debug MSG fCheckMails-08-{$keyID}: {$this->newestMailID}, {$this->maxID}");}
                 $updateMaxID = true;
 
                 // set the maxID
                 if ($updateMaxID) {
-                    setPermCache("newestCorpMailID", $this->maxID);
+                    setPermCache("newestCorpMailID{$this->keyID}", $this->maxID);
                 }
             }
         }
         $this->logger->addInfo("Next Mail Check At: {$cacheTimer} EVE Time");
+		if (DEBUG) {$this->logger->addInfo("Debug MSG fCheckMails-09-{$keyID}: Done..");}
     }
 
     /**
