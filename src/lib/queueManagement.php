0b96b153a11187cfbc5d479d7d4ba535d56a231b
shibdib <supernfadmin@gmail.com>
2016-11-19 18:41:17 +0430

    close #91

--- src/lib/queueManagement.php
+++ src/lib/queueManagement.php
@@ -41,7 +41,7 @@ function getQueuedMessage($id)
 
 function clearQueuedMessages($id)
 {
-    dbQueryRow("DELETE from messageQueue where id <= :id", array(":id" => $id));
+    dbQueryRow("DELETE from messageQueue where id = :id", array(":id" => $id));
     return null;
 }
 
