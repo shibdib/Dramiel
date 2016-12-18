d63c0d0385b37fcfc963ccd9c163949c9f2d338c
shibdib <supernfadmin@gmail.com>
2016-12-18 19:23:43 +0430

    trying to fix #127

--- src/plugins/onTick/siphons.php
+++ src/plugins/onTick/siphons.php
@@ -123,7 +123,7 @@ class siphons
                                     continue;
                                 }
                                 //Check for a multiple of 50 in the difference
-                                if ((int)$gooDifference % 50 !== 0) {
+                                if ($gooDifference % 50 !== 0) {
                                     $gooType = apiTypeName($silo->attributes()->typeID);
                                     $systemName = systemName($structures->attributes()->locationID);
                                     $msg = "{$siphonCorp['prefix']}";
@@ -155,7 +155,7 @@ class siphons
                                     continue;
                                 }
                                 //Check for a multiple of 50 in the difference
-                                if ((int)$gooDifference % 50 !== 0) {
+                                if ($gooDifference % 50 !== 0) {
                                     $gooType = apiTypeName($coupling->attributes()->typeID);
                                     $systemName = systemName($structures->attributes()->locationID);
                                     $msg = "{$siphonCorp['prefix']}";
