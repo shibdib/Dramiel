b7f4ece20247f7be6aa398515592c9f1ad964a4d
shibdib <supernfadmin@gmail.com>
2016-12-23 00:58:19 +0430

    remove case sensitivity for help

--- src/plugins/onMessage/help.php
+++ src/plugins/onMessage/help.php
@@ -88,7 +88,7 @@ class help
                 $this->logger->addInfo("Help: Sending help info to {$user}");
             } else {
                 foreach ($plugins as $plugin) {
-                    if ($messageString === $plugin->information()['name']) {
+                    if (strtolower($messageString) === strtolower($plugin->information()['name'])) {
                         $this->message->reply($plugin->information()['information']);
                     }
                 }
