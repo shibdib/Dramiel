8854a239b8adf125fabad7f645dffdc836b892fc
shibdib <supernfadmin@gmail.com>
2016-12-25 14:25:53 +0430

    initial standings based auth commit

--- src/lib/Db.php
+++ src/lib/Db.php
@@ -155,3 +155,104 @@ function dbExecute($query, array $params = array())
         $stmt->closeCursor();
     }
 }
+
+//MESSAGE QUEUE
+function queueMessage($message, $channel, $guild)
+{
+    dbExecute('REPLACE INTO messageQueue (`message`, `channel`, `guild`) VALUES (:message,:channel,:guild)', array(':message' => $message, ':channel' => $channel, ':guild' => $guild));
+    return null;
+}
+
+function getQueuedMessage($id)
+{
+    return dbQueryRow('SELECT * FROM messageQueue WHERE `id` = :id', array(':id' => $id));
+}
+
+function clearQueuedMessages($id)
+{
+    dbQueryRow('DELETE from messageQueue where id = :id', array(':id' => $id));
+    return null;
+}
+
+function getOldestMessage()
+{
+    return dbQueryRow('SELECT MIN(id) from messageQueue');
+}
+
+function priorityQueueMessage($message, $channel, $guild)
+{
+    $currentOldest = getOldestMessage();
+    $id = $currentOldest['MIN(id)'] - 1;
+    dbExecute('REPLACE INTO messageQueue (`id`, `message`, `channel`, `guild`) VALUES (:id,:message,:channel,:guild)', array(':id' => $id, ':message' => $message, ':channel' => $channel, ':guild' => $guild));
+}
+
+
+//RENAME QUEUE
+function queueRename($discordID, $nick, $guild)
+{
+    dbExecute('REPLACE INTO renameQueue (`discordID`, `nick`, `guild`) VALUES (:discordID,:nick,:guild)', array(':discordID' => $discordID, ':nick' => $nick, ':guild' => $guild));
+}
+
+function getQueuedRename($id)
+{
+    return dbQueryRow('SELECT * FROM renameQueue WHERE `id` = :id', array(':id' => $id));
+}
+
+function getOldestRename()
+{
+    return dbQueryRow('SELECT MIN(id) from renameQueue');
+}
+
+function clearQueuedRename($id)
+{
+    dbQueryRow('DELETE from renameQueue where id = :id', array(':id' => $id));
+    return null;
+}
+
+//
+function clearQueueCheck()
+{
+    $result = dbQueryRow('SELECT * FROM messageQueue');
+    if (@$result->num_rows > 35) {
+        clearAllMessageQueue();
+    }
+    return null;
+}
+
+//Clear Queue
+function clearAllMessageQueue()
+{
+    dbQueryRow('DELETE from messageQueue');
+    return null;
+}
+
+//CORP INFO
+function addCorpInfo($corpID, $corpTicker, $corpName)
+{
+    dbExecute('REPLACE INTO corpCache (`corpID`, `corpTicker`, `corpName`) VALUES (:corpID,:corpTicker,:corpName)', array(':corpID' => $corpID, ':corpTicker' => $corpTicker, ':corpName' => $corpName));
+}
+
+function getCorpInfo($corpID)
+{
+    return dbQueryRow('SELECT * FROM corpCache WHERE `corpID` = :corpID', array(':corpID' => $corpID));
+}
+
+function deleteCorpInfo($corpID)
+{
+    return dbQueryRow('DELETE from corpCache WHERE `corpID` = :corpID', array(':corpID' => $corpID));
+}
+
+//Remove old DB's
+function dbPrune()
+{
+    $oldDatabases = array('corpIDs', 'users', 'usersSeen');
+    foreach ($oldDatabases as $db) {
+        dbExecute("DROP TABLE IF EXISTS $db");
+    }
+}
+
+//Add Contacts
+function addContactInfo($contactID, $contactName, $standing)
+{
+    dbExecute('REPLACE INTO contactList (`contactID`, `contactName`, `standing`) VALUES (:contactID,:contactName,:standing)', array(':contactID' => $contactID, ':contactName' => $contactName, ':standing' => $standing));
+}
