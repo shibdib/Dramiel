f46595314e303d0977fbffd1da34efa9eef77f6d
Adam Algaert <adam.algaert@gmail.com>
2017-03-15 19:21:50 +0000

    Fix new citadel notification

--- src/plugins/onTick/notifications.php
+++ src/plugins/onTick/notifications.php
@@ -554,7 +554,8 @@ class notifications
                             }
                             break;
                         case 182: //  Citadel being anchored
-                            $corpName = trim(explode(': ', $notificationArray[1])[1]);
+                            preg_match('/(?<=ownerCorpName: )[^\r\n]+/i', $notificationString, $corpName);
+                            $corpName = $corpName[0];
                             preg_match('/(?<=solarsystemID: )\S+/i', $notificationString, $solarSystemID);
                             $systemName = systemName($solarSystemID[0]);
                             $msg = "Citadel owned by **{$corpName}** is being anchored in **{$systemName}**.";
