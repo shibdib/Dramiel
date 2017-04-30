41128c75a273fe73e250fe971f847a05fe0a1b14
shibdib <supernfadmin@gmail.com>
2017-04-30 15:47:01 -0400

    fix the phantom role removal

--- src/plugins/onTick/authCheck.php
+++ src/plugins/onTick/authCheck.php
@@ -323,7 +323,7 @@ class authCheck
             if (null === $result) {
                 $userCount++;
                 foreach ($roles as $role) {
-                    if ($id !== $botID && !in_array($role->name, $this->exempt, true)) {
+                    if ($id !== $botID && !in_array($role->name, $this->exempt, true) && $role->name !== '' || ' ' || null) {
                         $member->removeRole($role);
                         $guild->members->save($member);
                         // Add users name to array
